6a04b644ce6b2146cba9d3a391dbb79a70c04e40
memorydead <153998419+memorydead@users.noreply.github.com>
2023-12-16 19:09:30 +0530

    Add files via upload
    Tool to Extract Payload.bin

---
+++ run.sh
@@ -0,0 +1,21 @@
+#Kernel Extractor Tool By @mem_reb
+#This tool will extract Kernel , boot.img and all other partitions from payload.bin
+pkg install python -y
+pip install --upgrade pip
+pip install protobuf
+apt update && apt upgrade -y
+termux-setup-storage
+mkdir /sdcard/Extracted
+unzip /sdcard/rom.zip /sdcard/Extracted
+cp ./payload_dumper.py /sdcard/Extracted
+cp ./update_metadata_pb2.py /sdcard/Extracted
+python /sdcard/Extracted/payload_dumper.py /sdcard/Extracted/payload.bin
+rm /sdcard/Extracted/payload.bin
+rm /sdcard/Extracted/update_metadata_pb2.py
+rm /sdcard/Extracted/payload_dumper.py
+./magiskboot unpack /sdcard/Extracted/boot.img
+echo Script Finished
+
+
+
+
