f8b00fcd4cf6c245110bf1d77421218db0eccfa6
memorydead <153998419+memorydead@users.noreply.github.com>
2023-12-16 19:19:48 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ pip install protobuf
 apt update && apt upgrade -y
 termux-setup-storage
 mkdir /sdcard/Extracted
-unzip /sdcard/rom.zip /sdcard/Extracted
+mv /sdcard/rom.zip /sdcard/Extracted
+unzip /sdcard/Extracted/rom.zip
 cp ./payload_dumper.py /sdcard/Extracted
 cp ./update_metadata_pb2.py /sdcard/Extracted
 python /sdcard/Extracted/payload_dumper.py /sdcard/Extracted/payload.bin
