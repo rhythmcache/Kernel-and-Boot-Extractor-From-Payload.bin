5683431898287ae2648b4f8507113177af74c356
memorydead <153998419+memorydead@users.noreply.github.com>
2023-12-16 19:52:06 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -10,12 +10,17 @@ unzip /sdcard/rom.zip -d /sdcard/Extracted/
 cp ./payload_dumper.py /sdcard/Extracted
 cp ./update_metadata_pb2.py /sdcard/Extracted
 python /sdcard/Extracted/payload_dumper.py /sdcard/Extracted/payload.bin
+chmod 777 ./magiskboot
+./magiskboot unpack ./boot.img
+mv ./* /sdcard/extracted
 rm /sdcard/Extracted/payload.bin
 rm /sdcard/Extracted/update_metadata_pb2.py
 rm /sdcard/Extracted/payload_dumper.py
-chmod 777 ./magiskboot
-./magiskboot unpack /sdcard/Extracted/boot.img
+rm /sdcard/Extracted/magiskboot
+
+
 echo Script Finished
+echo your extracted stuffs are in /sdcard/Extracted
 
 
 
