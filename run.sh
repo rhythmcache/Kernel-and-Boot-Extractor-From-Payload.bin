2fbd04d599d5b3b635598d2e9cb2d3334ba57cbd
memorydead <153998419+memorydead@users.noreply.github.com>
2023-12-16 20:16:12 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -14,12 +14,11 @@ chmod 777 ./magiskboot
 ./magiskboot unpack ./boot.img
 mv ./* /sdcard/extracted
 rm /sdcard/Extracted/payload.bin
-rm /sdcard/Extracted/update_metadata_pb2.py
-rm /sdcard/Extracted/payload_dumper.py
-rm /sdcard/Extracted/magiskboot
+mv /sdcard/Extracted/update_metadata_pb2.py ./
+mv /sdcard/Extracted/payload_dumper.py ./
+mv /sdcard/Extracted/magiskboot ./
+mv /sdcard/Extracted/run.sh ./ 
 cd ~
-rmdir --ignore-fail-on-non-empty ./Kernel-and-Boot-Extractor-From-Payload.bin
-
 echo Script Finished
 echo your extracted stuffs are in /sdcard/Extracted
 
